648da8f60118dd489c160f308ea1e9eaa5d48fd9
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-02-29 15:05:28 +0300

    add plagin logic

--- script.js
+++ script.js
@@ -2,19 +2,47 @@ const btn = document.querySelector('.btn')
 const macroInput = document.querySelector('.macroInput')
 const macroContainer = document.querySelector('.macroContainer')
 
-const addMacro = () => {
-  const div = document.createElement('div')
-  div.classList.add('macro')
-  macroContainer.appendChild(div)
-  const p = document.createElement('p')
-  p.classList.add('macroText')
-  div.appendChild(p)
-  p.textContent = macroInput.value
-  const deleteBtn = document.createElement('button')
-  deleteBtn.classList.add('deleteBtn')
-  deleteBtn.textContent = 'X'
-  deleteBtn.addEventListener('click', () => div.remove())
-  div.appendChild(deleteBtn)
+const draw = () => {
+  macroContainer.innerHTML = ''
+  if (!localStorage.getItem('macro'))
+    localStorage.setItem('macro', JSON.stringify([]))
+  const data = JSON.parse(localStorage.getItem('macro'))
+  data.forEach((el) => {
+    const div = document.createElement('div')
+    macroContainer.appendChild(div)
+    div.classList.add('macro')
+    const p = document.createElement('p')
+    p.classList.add('macroText')
+    div.appendChild(p)
+    p.textContent = el
+    const deleteBtn = document.createElement('button')
+    deleteBtn.classList.add('deleteBtn')
+    deleteBtn.textContent = 'X'
+    deleteBtn.addEventListener('click', () => deleteFn(div, p))
+    div.appendChild(deleteBtn)
+  })
 }
+
+const deleteFn = (el, elText) => {
+  const data = JSON.parse(localStorage.getItem('macro')).filter(
+    (elem) => elem != elText.textContent,
+  )
+  localStorage.setItem('macro', JSON.stringify(data))
+  el.remove()
+}
+
+const addMacro = () => {
+  if (macroInput.value) {
+    if (!localStorage.getItem('macro'))
+      localStorage.setItem('macro', JSON.stringify([]))
+    const data = JSON.parse(localStorage.getItem('macro'))
+    data.push(macroInput.value)
+    localStorage.setItem('macro', JSON.stringify(data))
+    draw()
+  }
+  macroInput.value = ''
+}
+
+draw()
 
 btn.addEventListener('click', addMacro)
